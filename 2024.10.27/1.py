1e74055d4e043e5ed04369ee5ab24899a119a0b1
Yana <yanapautina9@gmail.com>
2024-10-28 19:18:45 +0700

    task_1

---
+++ 2024.10.27/1.py
@@ -0,0 +1,5 @@
+name = input("Введите Ваше имя: ")
+surname = input("Введите Вашу фамилию: ")
+year = int(input("Введите год Вашего рождения: "))
+print(name, surname + ',', 2024 - year)
+#Yana Pautina, 25
