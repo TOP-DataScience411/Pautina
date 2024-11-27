307271fe269989f359af65c74869841d26a23d88
Gennadiy <renardes@gmail.com>
2024-11-28 04:54:32 +0500

    2024.10.27: комментарии, правки, итоги

--- 2024.10.27/1.py
+++ 2024.10.27/1.py
@@ -2,4 +2,10 @@ name = input("Введите Ваше имя: ")
 surname = input("Введите Вашу фамилию: ")
 year = int(input("Введите год Вашего рождения: "))
 print(name, surname + ',', 2024 - year)
+
+
 #Yana Pautina, 25
+
+
+# ИТОГ: отлично — 2/2
+
