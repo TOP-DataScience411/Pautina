27781dbd70c5446bbf3abdd98c1dc88bdcdc3054
Yana <yanapautina9@gmail.com>
2024-12-18 07:39:39 +0700

    добавлено описание функции

--- 2024.12.14/1.py
+++ 2024.12.14/1.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 
 def list_files(absolutepath: str) -> tuple[str, str] | None:
+	"""Функция возвращает кортеж с именами файлов в каталоге по переданному пути,
+	если по переданному пути отсутствует каталог, функция возвращает None"""
 	pathdate = Path(absolutepath)
 	return tuple([file.name for file in pathdate.iterdir() if file.is_file()]) if pathdate.is_dir() else None
 
