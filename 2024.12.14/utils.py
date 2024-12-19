758172f20aebc522b3538f0e480619084547a760
Yana <yanapautina9@gmail.com>
2024-12-19 21:29:50 +0700

    Task_4

--- 2024.12.14/utils.py
+++ 2024.12.14/utils.py
@@ -1,13 +1,19 @@
 from shutil import copy2
-from sys import path
 from pathlib import Path
+import string
 
 
 def load_file(path: Path) -> Path:
 
 	"""Функция копирует файл в текущий каталог"""
 
-    return copy2(path, Path.cwd()/ path.name)
+	return copy2(path, Path.cwd()/ path.name)
 
 
+    
+def remove_punctuation(text: str) -> str:
 
+	"""Функция, удалающая знаки препинания из текста"""
+
+	translator = str.maketrans('', '', string.punctuation)
+	return text.translate(translator)
